c5cc3459d3ae9542041445da0533220444cc4460
Albin Kerouanton <akerouanton@compagnie-hyperactive.com>
2013-12-23 14:09:03 +0100

    Ajout des methodes getHome, is64BitSystem, isInstalled, isJavaInstalled et hasCompatLib.

--- Connection/OsSpecific/DebianConnection.php
+++ Connection/OsSpecific/DebianConnection.php
@@ -7,6 +7,20 @@ use DP\PHPSeclibWrapperBundle\Connection\OsSpecific\OsSpecificConnectionInterfac
 
 class DebianConnection extends Connection implements OsSpecificConnectionInterface
 {
+    protected $home;
+    
+    /**
+     * @{inheritDoc}
+     */
+    public function getHome()
+    {
+        if (!isset($this->home)) {
+            $this->home = $this->getSSH()->exec('cd ~ && pwd');
+        }
+        
+        return $this->home;
+    }
+    
     /**
      * @{inheritDoc}
      */
@@ -20,7 +34,6 @@ class DebianConnection extends Connection implements OsSpecificConnectionInterfa
             $cmd = 'touch -t ' . $mtime . ' ' . $filepath; 
         }
         
-        // La commande ne renvoie rien
         return $this->getSSH()->exec($cmd) == '';
     }
     
@@ -30,5 +43,48 @@ class DebianConnection extends Connection implements OsSpecificConnectionInterfa
     public function createDirectory($dirpath)
     {
         return $this->getSSH()->exec('mkdir ' . $dirpath) == '';
+    }
+    
+    /**
+     * @{inheritDoc}
+     */
+    public function is64BitSystem()
+    {
+        return strlen($this->getSSH()->exec('uname -r | grep "\-64"')) > 0;
+    }
+    
+    /**
+     * @{inheritDoc}
+     */
+    public function isInstalled($packet)
+    {
+        $ret = trim($this->exec('dpkg-query -W --showformat=\'${Status}\n\' ' . $packet . ' | grep \'install ok installed\''));
+        
+        return $ret == 'install ok installed';
+    }
+    
+    /**
+     * @{inheritDoc}
+     */
+    public function isJavaInstalled()
+    {
+        return strlen($this->getSSH()->exec('type java 2>/dev/null')) > 0;
+    }
+    
+    /**
+     * @{inheritDoc}
+     */
+    public function hasCompatLib()
+    {
+        // On récupère la version du système debian utilisé
+        // puisque le paquet à vérifier diffère avec la debian wheezy
+        $os_version = floatval(trim($this->getSSH()->exec('cat /etc/debian_version')));
+        
+        if ($os_version >= 7) {
+            return $this->isInstalled('libc6:i386');
+        }
+        else {
+            return $this->isInstalled('ia32-libs');
+        }
     }
 }
--- Connection/OsSpecific/OsSpecificConnectionInterface.php
+++ Connection/OsSpecific/OsSpecificConnectionInterface.php
@@ -8,6 +8,16 @@ use DP\PHPSeclibWrapperBundle\Connection\OsSpecific\Exception\UnavailableMethodE
 interface OsSpecificConnectionInterface
 {
     /**
+     * Gets the user home
+     * 
+     * @throws MethodNotImplementedException
+     * @throws UnavailableMethodException
+     * 
+     * @return string
+     */
+    public function getHome();
+    
+    /**
      * Creates $filepath or modify its modification time
      * 
      * @param $filepath string    File path of the file to create or to update
@@ -31,4 +41,44 @@ interface OsSpecificConnectionInterface
      * @return boolean
      */
     public function createDirectory($dirpath);
+    
+    /**
+     * Determine whether the os is a 64 bit system
+     * 
+     * @throws MethodNotImplementedException
+     * @throws UnavailableMethodException
+     * 
+     * @return boolean
+     */
+    public function is64BitSystem();
+    
+    /**
+     * Determine whether the $program is installed
+     * 
+     * @throws MethodNotImplementedException
+     * @throws UnavailableMethodException
+     * 
+     * @return boolean
+     */
+    public function isInstalled($program);
+    
+    /**
+     * Determine whether java is installed
+     * 
+     * @throws MethodNotImplementedException
+     * @throws UnavailableMethodException
+     * 
+     * @return boolean
+     */
+    public function isJavaInstalled();
+    
+    /**
+     * Determine whether if the 32/64 bits compatability library is installed (ia32-libs)
+     * 
+     * @throws MethodNotImplementedException
+     * @throws UnavailableMethodException
+     * 
+     * @return boolean
+     */
+    public function hasCompatLib();
 }

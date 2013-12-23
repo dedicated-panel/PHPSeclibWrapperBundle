c2a51c17a4b71a6db5c0b970ecaa7bbe704ad03d
Albin Kerouanton <akerouanton@compagnie-hyperactive.com>
2013-12-23 11:23:08 +0100

    Correction indentation phpunit.xml.dist + ajout parametres debug PHPSeclib.

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -5,4 +5,10 @@ if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
 }
 
-require_once $file;
+$loader = require $file;
+
+$loader->loadClass('Net_SSH');
+$loader->loadClass('Net_SFTP');
+
+define('NET_SSH_LOGGING',  NET_SSH2_LOG_COMPLEX);
+define('NET_SFTP_LOGGING', NET_SSH2_LOG_COMPLEX);
